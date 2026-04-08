2bb90614b24c095b1b9272cadb2dafd4b40c5b65
Imngzx <117638365+Imngzx@users.noreply.github.com>
2026-04-08 20:56:21 +0800

    fix(code): made my code runs

--- main.c
+++ main.c
@@ -7,14 +7,39 @@
 #include <unistd.h>
 
 #define PORT 9090
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 2048
+
+char *parseRoute(const char *route) {
+  if (strcmp(route, "/") == 0 || strcmp(route, "/home") == 0) {
+    return "static/index.html";
+  } else if (strcmp(route, "/about") == 0) {
+    return "static/about.html";
+  } else if (strcmp(route, "/favicon.ico") == 0) {
+    return "static/favicon.ico";
+  }
+
+  return "static/notFound.html";
+}
 
 void sendHTML(int *sock, const char *file) {
   FILE *html = fopen(file, "r");
   if (!html) {
     perror("could not open HTML file");
+    char *errorMsg = "HTTP/1.1 404 Not Found\r\n\r\nFile Not Found on Server.";
+    send(*sock, errorMsg, strlen(errorMsg), 0);
     return;
   }
+
+  char buffer[BUFFER_SIZE] = {0};
+  size_t read = 0;
+
+  char *header = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+  send(*sock, header, strlen(header), 0);
+
+  while ((read = fread(buffer, sizeof(char), BUFFER_SIZE, html)) > 0) {
+    send(*sock, buffer, read, 0);
+  }
+
   fclose(html);
 }
 
@@ -24,6 +49,9 @@ int main() {
     perror("could not get our serversocket fd");
     return -1;
   }
+
+  int opt = 1;
+  setsockopt(serverSock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
   struct sockaddr_in serverAddr;
   serverAddr.sin_family = AF_INET;
@@ -36,7 +64,6 @@ int main() {
   }
 
   if (listen(serverSock, 5) < 0) {
-
     perror("could not listen on server socket and ip");
   }
 
@@ -50,16 +77,29 @@ int main() {
     if ((*clientSocket = accept(serverSock, (struct sockaddr *)&clientAddr,
                                 &clientLen)) < 0) {
       perror("could not accept client");
+      free(clientSocket);
       continue;
     }
-    printf("client connected");
-    char *msg = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello World!\n";
-    send(*clientSocket, msg, strlen(msg), 0);
+    printf("client connected\n");
+
+    char recBuf[BUFFER_SIZE] = {0};
+    recv(*clientSocket, recBuf, BUFFER_SIZE - 1, 0);
+    printf("%s\n", recBuf);
+
+    char method[16] = {0};
+    char path[256] = {0};
+
+    if (sscanf(recBuf, "%15s %255s", method, path) == 2) {
+      printf("Requested Path: %s\n", path);
+
+      sendHTML(clientSocket, parseRoute(path));
+    }
+
     close(*clientSocket);
-    printf("client disconnected");
+    printf("client disconnected\n\n");
     free(clientSocket);
   }
-  close(serverSock);
 
+  close(serverSock);
   return 0;
 }
